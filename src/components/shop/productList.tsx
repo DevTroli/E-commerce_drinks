2111e0bf9ec0d0ee1bb09c76bd43779a62e9fa42
Troli <brawlixo123@gmail.com>
2023-11-26 14:36:49 -0300

    api routes fixing

--- src/components/shop/productList.tsx
+++ src/components/shop/productList.tsx
@@ -1,5 +1,28 @@
-import { getProducts } from "@/app/api/route"
 import ProductCard from "@/components/shop/productCard"
+import stripe from "@/lib/stripe"
+import Stripe from "stripe"
+
+async function getProducts() {
+  try{
+    const StripeProducts = await stripe.products.list({
+      expand: ['data.default_price'],
+    })
+    console.log(StripeProducts)
+    return StripeProducts.data.map((p:Stripe.Product) => {
+      return {
+        id: p.id.toString(),
+        name: p.name,
+        description: p.description ?? '',
+        price: (p.default_price as Stripe.Price).unit_amount_decimal ?? '0',
+        currency: (p.default_price as Stripe.Price)?.currency ?? 'BRL',
+        images: p.images,
+        image: p.images[0],
+      }
+    })
+  }catch(e: any) {
+    console.log("Stripe deu erro olha lá hein", e.message)
+  }
+}
 
 export default async function ProductList() {
   const products = await getProducts()
